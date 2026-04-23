c9c94d647e159a99c4e5e2458c8ea8c1fa5a8591
FelipeSouza <felipesouza0500@gmail.com>
2026-04-23 10:22:23 -0300

    Implementado o CRON, comentarios e login ded admin

--- src/main/java/com/felipesouza/bff_agendador_tarefas/BffAgendadorTarefasApplication.java
+++ src/main/java/com/felipesouza/bff_agendador_tarefas/BffAgendadorTarefasApplication.java
@@ -3,9 +3,11 @@ package com.felipesouza.bff_agendador_tarefas;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication	//Indica que é uma aplicação do spring e faz as configurações necessárias
 @EnableFeignClients		//Habilita o uso do FeignClient para usar os metodos de requisições HTTP de outros microserviços
+@EnableScheduling		//Habilita o uso do Scheduled para usar o agendamento de tarefas
 public class BffAgendadorTarefasApplication {
 
 	public static void main(String[] args) {
@@ -13,3 +15,5 @@ public class BffAgendadorTarefasApplication {
 	}
 
 }
+
+//Após iniciar os servidores, usar a seguinte url: http://localhost:8083/swagger-ui/index.html
--- src/main/java/com/felipesouza/bff_agendador_tarefas/infrastructure/client/EmailClient.java
+++ src/main/java/com/felipesouza/bff_agendador_tarefas/infrastructure/client/EmailClient.java
@@ -4,6 +4,7 @@ package com.felipesouza.bff_agendador_tarefas.infrastructure.client;
 
 import com.felipesouza.bff_agendador_tarefas.business.dto.out.TarefasDTOResponse;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 /* O OpenFeign é um cliente HTTP para a comunição entre microserviços.
@@ -14,5 +15,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 @FeignClient(name = "notificacao", url = "${notificacao.url}")
 public interface EmailClient {
 
+    @PostMapping
     void enviarEmail(@RequestBody TarefasDTOResponse dto);
 }
