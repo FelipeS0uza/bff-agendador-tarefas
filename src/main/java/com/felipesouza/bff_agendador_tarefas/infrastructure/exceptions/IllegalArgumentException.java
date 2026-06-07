557901cab289a78b965a07af0681c0bc3bc4b95e
FelipeSouza <felipesouza0500@gmail.com>
2026-06-07 19:24:26 -0300

    Adicionado endpoint para api de busca de cep

---
+++ src/main/java/com/felipesouza/bff_agendador_tarefas/infrastructure/exceptions/IllegalArgumentException.java
@@ -0,0 +1,12 @@
+package com.felipesouza.bff_agendador_tarefas.infrastructure.exceptions;
+
+public class IllegalArgumentException extends RuntimeException {
+
+    public IllegalArgumentException(String message) {
+        super(message);
+    }
+
+    public IllegalArgumentException(String message, Throwable throwable) {
+        super(message, throwable);
+    }
+}
